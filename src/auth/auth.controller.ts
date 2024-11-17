2b055856ca349ba62e2ce7745bca0435a4adaf94
madicke12 <cissemadicke8@gmail.com>
2024-11-17 19:40:35 +0000

    feat: implement authentication endpoints for login, logout, and signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthSignUpDto, AuthSignInDto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authservice: AuthService) {}
+  @Post('login')
+  login(@Body() dto: AuthSignInDto) {
+    return this.authservice.login(dto);
+  }
+  @Post('logout')
+  logout() {
+    return this.authservice.logout();
+  }
+  @Post('signup')
+  signup(@Body() dto: AuthSignUpDto) {
+    return this.authservice.signup(dto);
+  }
+}
